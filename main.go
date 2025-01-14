2039677412a8232e00f8808bed586545f660ba62
Abhishek <programwithabhi@gmail.com>
2025-01-14 11:00:51 +0530

    How to make closure in GO
    The inner function will have access to it's outer scope variables when
    it it created

--- main.go
+++ main.go
@@ -10,11 +10,20 @@ func transformNumbers(numbers *[]int, transform func(int) int ) []int {
 	return transformedNumbers
 }
 
+func createTransformFunction(factor int) func(int) int {
+  return func (number int) int {
+    return number * factor
+  }
+}
+
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 	doubledNumbers := transformNumbers(&numbers, func(number int) int {
     return number * 2; 
-  })
+  });
+  tripleTransformFn := createTransformFunction(3);
+  tripledNumbers := transformNumbers(&numbers, tripleTransformFn)
 
 	fmt.Println("Doubled numbers is:", doubledNumbers)
+	fmt.Println("Tripled numbers is:", tripledNumbers)
 }
