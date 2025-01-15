3b8fdc2f405329de6bcaf332bcfc005c78d76436
Abhishek <programwithabhi@gmail.com>
2025-01-15 13:33:52 +0530

    How we can pass the list to a variadic function
    How we can pass the list to function with the use of spread operator
    where it wants the same type of sandalone parameters with collect all
    syntax

--- variadicFunctions/main.go
+++ variadicFunctions/main.go
@@ -3,16 +3,16 @@ package variadicFunctions
 import "fmt"
 
 //This functions is accpeting individual integer parameters and convert it into slice and store onto numbers variable (That's how the function is variadic)
-func sum(numbers ...int) int {
+func sum(firstParameter int, numbers ...int) int {
   sum := 0;
   for _, val := range numbers {
     sum += val;
   }
-  return sum;
+  return sum + firstParameter;
 }
 
 func VariadicFunctionsMainFn(){
-  //numbers := []int{1, 2, 3, 4};
-  sumOfNumbers := sum(1, 2, 3, 4);
+  numbers := []int{1, 2, 3, 4};
+  sumOfNumbers := sum(5, numbers...);
   fmt.Println("Sum of the numbers is:", sumOfNumbers);
 }
